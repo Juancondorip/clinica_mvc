ff6ccbf2386e8941ecc5aa704ad294d1ab662959
Juancondorip <2001condoripayi@gmail.com>
2026-05-22 17:19:43 -0400

    Corrijo run.py para Render

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import create_app, db
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-
+# Solo crear tablas si estás en local, no en producción
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
